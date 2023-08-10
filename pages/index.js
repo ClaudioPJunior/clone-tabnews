e8ff38e613a51c4384c13131146f138f21721db9
Cláudio P. Júnior <claudio_pulim@hotmail.com>
2023-08-10 22:28:41 +0000

    Modificando a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() 
 {
-    return <h1>Mor, eu te amo!! Essa é a primeira frase escrita de um pedaço da internet só nosso! Se gostou e me ama também, da um sorrisinho!! rs' 💕❤️💕</h1>
+    return <h1>Mor, eu te amo!! Essa é a primeira frase, uma declaração, escrita em um pedaço da internet só nosso! Se gostou e me ama também, me manda uma foto sorrindo!! rs' 💕❤️💕</h1>
 }
 
 export default Home;
